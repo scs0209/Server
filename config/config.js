c847c67255739a7d686965da19ec899c230e7e29
Changsu Seong <110822847+scs0209@users.noreply.github.com>
2024-05-09 14:26:48 +0900

    fix: 서버 수정

--- config/config.js
+++ config/config.js
@@ -4,16 +4,16 @@ module.exports = {
   development: {
     username: 'tutorial_user',
     password: process.env.MYSQL_PASSWORD,
-    database: 'sample',
-    host: 'db-instance.clfzc3a1xomr.ap-northeast-2.rds.amazonaws.com',
+    database: 'blog',
+    host: '127.0.0.1',
     port: '3306',
     dialect: 'mysql',
   },
   production: {
     username: 'tutorial_user',
     password: process.env.MYSQL_PASSWORD,
-    database: 'sample',
-    host: 'db-instance.clfzc3a1xomr.ap-northeast-2.rds.amazonaws.com',
+    database: 'blog',
+    host: '127.0.0.1',
     dialect: 'mysql',
   },
 };
